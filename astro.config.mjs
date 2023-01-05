63ef1ab358cb8af368f50bbac9b84db5195e9354
francesco manenti <francesco@manenti.net>
2023-01-05 14:56:37 +0100

    change astro.config and add deploy workflow to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://framik76.github.io',
+    base: '/casavacanze-lavecchiafonte-website',
+});
