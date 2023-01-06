260a5c363cc7ef8fe7f54c928af6cf9865aa2be6
francesco manenti <francesco@manenti.net>
2023-01-06 11:21:13 +0100

    remove mapbox deps and add google maps iframe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://framik76.github.io',
-    base: '/casavacanze-lavecchiafonte-website',
+    base: '/casavacanze-lavecchiafonte-website/',
+    integrations: [react()]
 });
