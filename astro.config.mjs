338f4a8e59856ade1bfa3a9ae3e240d6c4ea0b99
francesco manenti <francesco@manenti.net>
2023-01-08 15:29:26 +0100

    change site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://framik76.github.io',
-    base: '/casavacanze-lavecchiafonte-website/',
+    site: 'https://casavacanzelavecchiafonte.it',
+    base: '/',
     integrations: [react()]
 });
